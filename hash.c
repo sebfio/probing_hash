d01a67afd0e9aef48553dc2f4120040e2f67561b
Sebastian Fiorini <sebf465@gmail.com>
2017-07-14 00:23:26 -0400

    Destroy added Converted to spaces

--- hash.c
+++ hash.c
@@ -4,38 +4,46 @@
 
 
 struct hashmap *hashmap_init (hash_function hash_fn,
-							  key_compare key_cmp,
-							  hash_dest) {
-	if (!hash_fn || !key_cmp || !hash_dest) {
-		// Invalid support for hash table functions
-		return NULL;
-	}
+                              hash_insert_key hash_key_fn,
+                              hash_destroy hash_kv_remove) {
+    if (!hash_fn || !key_cmp || !hash_dest) {
+        // Invalid support for hash table functions
+        return NULL;
+    }
 
-	struct hashmap *hasht = malloc(sizeof(struct hashmap));
+    struct hashmap *hasht = malloc(sizeof(struct hashmap));
 
-	if (!hasht)
-		return NULL;
+    if (!hasht)
+        return NULL;
 
-	hasht->table_size = 0;
-	hasht->num_entries = 0;
-	hasht->kv_table = malloc(sizeof(void *) * NEW_TABLE_SIZE);
+    hasht->table_size = 0;
+    hasht->num_entries = 0;
+    hasht->kv_table = calloc(sizeof(kv), NEW_TABLE_SIZE);
 
-	if (!hasht->table) {
-		free(hasht)
-		return NULL;
-	}
+    if (!hasht->table) {
+        free(hasht)
+        return NULL;
+    }
 
-	hasht->hash_fn 		= hash_fn;
-	hasht->key_cmp 		= key_cmp;
-	hasht->hash_dest 	= hash_dest;
+    hasht->hash_fn      = hash_fn;
+    hasht->key_fn       = hash_key_fn;
+    hasht->kv_remove    = hash_kv_remove;
 
-	return hasht;
+    return hasht;
 }
 
-int32_t hashmap_destroy (struct hashmap *self);
+int32_t hashmap_destroy (struct hashmap *self) {
+    for (uint32_t i = 0; i < self->table_size; i++) {
+        if (self->hasht->kv_table[i]) {
+            self->kv_remove(&self->hasht->kv_table[i]);
+        }
+    }
+    
+    free(self);
+}
 
 int32_t hashmap_put (struct hashmap *self,
-					 const struct *kv);
+                     const struct *kv);
 
 const void* hashmap_get (struct hashmap *self, const void *key);
 
--- hash.h
+++ hash.h
@@ -3,37 +3,38 @@
 
 #include <stdint.h>
 
-#define NEW_TABLE_SIZE 100
+#define NEW_TABLE_SIZE 97
 
 struct kv {
-	void *k;
-	void *v;
+    void *k;
+    void *v;
 }
 
-typedef int32_t (*hash_functionn)(struct hashmap *self, struct kv);
-typedef int32_t (*key_compare)(struct hashmap *self,
-					   	   struct kv *entry1,
-					   	   struct kv *entry2);
-typedef int32_t (*hash_destroy)(struct hashmap *self);
+typedef int32_t (*hash_function)(void *key);
+typedef int32_t (*hash_insert_key)(struct *kv);
+typedef int32_t (*hash_destroy)(struct *kv);
 
 struct hashmap {
-	size_t table_size;
-	size_t num_entries;
-	void *table;
-	hash_function 	hash_fn;
-	key_compare		key_cmp;
-	hash_destroy	hash_dest;
+    size_t table_size;
+    size_t num_entries;
+    void *table;
+    // Hash function for every key in table
+    hash_function   hash_fn;
+    // How keys should be inserted
+    hash_insert_key key_fn;
+    // How a key should be removed
+    hash_destroy    kv_remove;
 }
 
 // CRUD functions
 struct hashmap *hashmap_init (hash_function hash_fn,
-							  key_compare key_cmp,
-							  hash_dest);
+                              hash_insert_key hash_key_fn,
+                              hash_destroy hash_kv_remove);
 
 int32_t hashmap_destroy (struct hashmap *self);
 
 int32_t hashmap_put (struct hashmap *self,
-					 const struct *kv);
+                     const struct *kv);
 
 const void* hashmap_get (struct hashmap *self, const void *key);
 
