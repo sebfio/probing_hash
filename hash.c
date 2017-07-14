ac04aa3282065569c81ecdfa6992a9eb048b990b
Sebastian Fiorini <sebf465@gmail.com>
2017-07-14 19:43:35 -0400

    Added functions for get remove

--- hash.c
+++ hash.c
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 
 #define RESIZE_LARGE_FACTOR 0.4
-#define RESIZE_SMALL_FACTOR 0.5
+#define RESIZE_SMALL_FACTOR 0.05
 
 typedef index int32_t;
 
-static index hash_find_first_free_index (struct hashmap *self, const void *key);
-static index hash_find_entered_index (struct hashmap *self, const void *key);
+static index hash_find_first_free_index (struct hashmap *self, void const *key);
 
 struct hashmap *hashmap_init (hash_function hash_fn,
                               hash_insert_key hash_key_fn,
-                              hash_destroy hash_kv_remove) {
+                              hash_destroy hash_kv_remove
+                              hash_compare hash_kk_compare) {
     if (!hash_fn || !key_cmp || !hash_dest) {
         // Invalid support for hash table functions
         return NULL;
@@ -33,10 +33,10 @@ struct hashmap *hashmap_init (hash_function hash_fn,
         return NULL;
     }
 
-    hasht->hash_fn      = hash_fn;
-    hasht->key_fn       = hash_key_fn;
-    hasht->kv_remove    = hash_kv_remove;
-
+    hasht->hash_fn          = hash_fn;
+    hasht->insert_key_fn    = hash_key_fn;
+    hasht->kv_remove        = hash_kv_remove;
+    hasht->kk_compare       = hash_kk_compare;
     return hasht;
 }
 
@@ -57,7 +57,7 @@ int32_t hashmap_destroy (struct hashmap *self) {
 }
 
 int32_t hashmap_put (struct hashmap *self,
-                     const struct *kv) {
+                     struct const *kv) {
     if (!self || !kv)
         return -1;
 
@@ -80,6 +80,69 @@ int32_t hashmap_put (struct hashmap *self,
     }
 }
 
-const void* hashmap_get (struct hashmap *self, const void *key);
+void * const hashmap_get (struct hashmap *self, void const *key) {
+    if (!self || !key)
+        return NULL;
 
-int32_t hashmap_remove (struct hashmap *self, const void *key);
+    struct kv *kv = hash_kv_find(self, key);
+
+    if (!kv)
+        return NULL; 
+
+    return kv->v;
+}
+
+int32_t hashmap_remove (struct hashmap *self, void const *key) {
+    if (!self || !key) {
+        return -1;
+    }
+
+    struct kv *kv = hash_kv_find(self, key);
+
+    if (!kv) {
+        return -1;
+    }
+
+    self->num_entries--;
+
+    if (self->num_entries * 1.0 / self->table_size < RESIZE_SMALL_FACTOR) {
+        // TODO: Resize table
+    }
+}
+
+static index hash_find_first_free_index (struct hashmap *self, void const *key) {
+    uint32_t const initial_index = self->hash_fn(key) % self->table_size;
+
+    for (uint32_t i = 0; i < self->table_size; i++) {
+        uint32_t key_location = (i + initial_index) / self->table_size;
+        
+        // Find the first free NULL key
+        if (!self->table[key_location].k)
+            return key_location;
+    }
+}
+
+static struct kv *hash_kv_find (struct hashmap *self, void const *key) {
+    int32_t key_hash = self->hash_fn(key);
+    uint32_t initial_index = key_hash % self->table_size;
+
+    for (uint32_t i = 0; i < self->table_size; i++) {
+        uint32_t key_location = (i + initial_index) / self->table_size;
+
+        if (!self->hash(self->table[key_location].k)) {
+            // Key does not exist
+            return NULL;
+        }
+
+        if (self->hash(self->table[key_location].k) != key_hash) {
+            continue;
+        }
+
+        // Check exact keys match, if so return index
+        if (self->kk_compare(self->table[key_location].k, key) == 0)
+            return &self->table[key_location];
+    }
+
+    // Should never get here
+    return NULL;
+}
--- hash.h
+++ hash.h
@@ -13,6 +13,7 @@ struct kv {
 typedef int32_t (*hash_function)(void *key);
 typedef int32_t (*hash_insert_key)(struct *kv);
 typedef int32_t (*hash_destroy)(struct *kv);
+typedef int32_t (*hash_compare)(struct const *kv1, struct const *kv2);
 
 struct hashmap {
     size_t table_size;
@@ -21,23 +22,27 @@ struct hashmap {
     // Hash function for every key in table
     hash_function   hash_fn;
     // How keys should be inserted
-    hash_insert_key key_fn;
+    hash_insert_key insert_key_fn;
     // How a key should be removed
     hash_destroy    kv_remove;
+    // Expensive compare to make sure keys do match before returning
+    // hit
+    hash_compare    kk_compare;
 }
 
 // CRUD functions
 struct hashmap *hashmap_init (hash_function hash_fn,
                               hash_insert_key hash_key_fn,
-                              hash_destroy hash_kv_remove);
+                              hash_destroy hash_kv_remove,
+                              hash_compare kk_compare);
 
 int32_t hashmap_destroy (struct hashmap *self);
 
 int32_t hashmap_put (struct hashmap *self,
-                     const struct *kv);
+                     struct const *kv);
 
-const void* hashmap_get (struct hashmap *self, const void *key);
+void * const hashmap_get (struct hashmap *self, void const *key);
 
-int32_t hashmap_remove (struct hashmap *self, const void *key);
+int32_t hashmap_remove (struct hashmap *self, void const *key);
 
 #endif // HASH_H_
